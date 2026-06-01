c83ee578d42aa4cd810c52aefeee76a5700e80ac
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-02 01:00:35 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,58 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color.fromARGB(255, 235, 182, 113),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20, bottom: 20, left: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                title: Text(
-                  'Flutter Tips',
-                  style: TextStyle(fontSize: 28, color: Colors.black),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: const Color.fromARGB(255, 235, 182, 113),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 20, left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  title: Text(
+                    'Flutter Tips',
+                    style: TextStyle(fontSize: 28, color: Colors.black),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 12, bottom: 12),
+                    child: Text(
+                      'Build your career with Nour Waleed',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black.withValues(alpha: 0.5),
+                      ),
+                    ),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      FontAwesomeIcons.trash,
+                      color: Colors.black,
+                      size: 28,
+                    ),
+                  ),
                 ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.only(top: 12, bottom: 12),
+                Padding(
+                  padding: const EdgeInsets.only(right: 24, top: 16),
                   child: Text(
-                    'Build your career with Nour Waleed',
+                    'May21, 2026',
                     style: TextStyle(
-                      fontSize: 18,
+                      fontSize: 16,
                       color: Colors.black.withValues(alpha: 0.5),
                     ),
                   ),
                 ),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    FontAwesomeIcons.trash,
-                    color: Colors.black,
-                    size: 28,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 24, top: 16),
-                child: Text(
-                  'May21, 2026',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black.withValues(alpha: 0.5),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
