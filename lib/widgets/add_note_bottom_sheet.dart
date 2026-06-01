17affa533c47217f31161db9eefb8e5c776106f5
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-02 00:52:19 +0300

    finish notes bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constant.dart';
+import 'package:note_app/widgets/custom_button.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,13 +10,18 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          SizedBox(height: 50),
-          CustomTextField(hint: 'title', maxLines: 1),
-          SizedBox(height: 16),
-          CustomTextField(hint: 'content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            CustomTextField(hint: 'title', maxLines: 1),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'content', maxLines: 5),
+            SizedBox(height: 32),
+            CustomBotton(),
+            SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/constant.dart';
+
+class CustomBotton extends StatelessWidget {
+  const CustomBotton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: KPrimaryColor,
+      ),
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
